de9a77bffc61707d7da7dcb20e5ed2ea53479bce
Nikhil <nikhil@kredx.com>
2018-03-24 22:39:33 +0530

    stable

---
+++ packages/universal_components/.storybook/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+// use babel-minify due to UglifyJS errors from modern JS syntax
+// get default webpack config for @storybook/react
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const DEV = process.env.NODE_ENV !== 'production';
+const prodPlugins = [
+    new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(
+            process.env.NODE_ENV || 'development'
+        ),
+        'process.env.__REACT_NATIVE_DEBUG_ENABLED__': DEV,
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+];
+module.exports = (baseConfig, env) => {
+    const config = genDefaultConfig(baseConfig, env);
+    const defaultPlugins = config.plugins;
+    const overwrite = {
+        devtool: 'inline-source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel-loader',
+                    query: { cacheDirectory: true },
+                },
+            ],
+        },
+        plugins: DEV ? defaultPlugins : prodPlugins,
+    };
+    return Object.assign(config, overwrite);
+};
